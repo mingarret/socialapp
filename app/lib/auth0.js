a7a40c0396200ab9725dc735b25f67140966baec
mingarret <ximomingarro@hotmail.com>
2025-01-28 21:49:05 +0100

    cambios en el archivo auth0.js, solucion de problema de despliegue en produccion

--- app/lib/auth0.js
+++ app/lib/auth0.js
@@ -1,3 +1,9 @@
-import { Auth0Client } from "@auth0/nextjs-auth0/server"
+import { Auth0Client } from "@auth0/nextjs-auth0/server";
 
-export const auth0 = new Auth0Client()
+export const auth0 = new Auth0Client({
+  secret: process.env.AUTH0_SECRET,
+  baseURL: process.env.AUTH0_BASE_URL,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+});
